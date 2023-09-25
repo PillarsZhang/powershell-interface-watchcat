133b2c7a95bdf1c0a445bd9338ddde8f11e79b85
Pillars Zhang <zhangkaihao2000@qq.com>
2023-09-26 02:30:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# [PowerShell] Interface WatchCat
+
+Check the availability of the network and automatically restart the network adapter. (Just like Openwrt's Watchcat - network watchdog utility)
+
+The Watch-Cat function continuously monitors network connectivity by pinging a specified address or default gateway. If the ping fails for a specified number of times, it can attempt to restart the network adapter to recover the connection.
+
+**Administrator privileges required!**
+
+# Quick Start
+
+```powershell
+git clone https://github.com/PillarsZhang/powershell-interface-watchcat.git
+cd .\powershell-interface-watchcat\
+Import-Module .\ -Force
+Get-Help Watch-Cat -Full
+Watch-Cat -WatchInterface "以太网"
+```
+
+# Install as module
+
+```powershell
+$ModulePath = "$Home\Documents\WindowsPowerShell\Modules\powershell-interface-watchcat\"
+New-Item -ItemType Directory -Path $ModulePath -Force
+Copy-Item -Path .\powershell-interface-watchcat.psm1 -Destination $ModulePath -Force
+
+Write-Host "To import the module globally, append follow line to ""$Home\Documents\WindowsPowerShell\profile.ps1"""
+Import-Module powershell-interface-watchcat
+```
+
+# Install as service
+
+Install as module first. Modify the `arguments` in `powershell-interface-watchcat.winsw.xml` and `PSModulePath` in `env`. [winsw v2](https://github.com/winsw/winsw/tree/master) required.
+
+```powershell
+# For most users
+$WinswUri = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW.NET461.exe"
+# $WinswUri = "https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW-x64.exe"
+Invoke-WebRequest -Uri $WinswUri -OutFile ".\powershell-interface-watchcat.winsw.exe"
+.\powershell-interface-watchcat.winsw.exe install
+.\powershell-interface-watchcat.winsw.exe start
+.\powershell-interface-watchcat.winsw.exe status
+```
---
+++ powershell-interface-watchcat.psm1
@@ -0,0 +1,136 @@
+﻿function Watch-Cat {
+<#
+.SYNOPSIS
+    Check the availability of the network and automatically restart the network adapter. (Just like Openwrt's WatchCat - network watchdog utility)
+
+.DESCRIPTION
+    The Watch-Cat function continuously monitors network connectivity by pinging a specified address or default gateway. If the ping fails for a specified number of times, it can attempt to restart the network adapter to recover the connection.
+
+.PARAMETER WatchInterface
+    Specifies the network interface to monitor. This can be the interface alias, interface index, or interface description.
+
+.PARAMETER PingAddress
+    Specifies the address to ping. If not provided, the default gateway of the specified interface will be used.
+
+.PARAMETER PingDelay
+    Specifies the delay in seconds between each ping attempt. The default value is 10 seconds.
+
+.PARAMETER MaxFailedAttempts
+    Specifies the maximum number of consecutive ping failures before attempting to restart the network adapter. The default value is 3.
+
+.PARAMETER MaxRestartAttempts
+    Specifies the maximum number of restart attempts to recover the connection. The default value is 3.
+
+.PARAMETER RestartDelay
+    Specifies the delay in seconds after restarting the network adapter. The default value is 30 seconds.
+
+.EXAMPLE
+    Watch-Cat -WatchInterface "以太网"
+    For users whose system language is Chinese, the specific interface name can be viewed by "Get-NetIPConfiguration" in PowerShell (InterfaceAlias).
+
+.EXAMPLE
+    Watch-Cat -WatchInterface "Ethernet" -PingAddress "8.8.8.8" -PingDelay 5 -MaxFailedAttempts 5 -MaxRestartAttempts 2 -RestartDelay 60
+    Monitors the "Ethernet" interface by pinging "8.8.8.8" every 5 seconds. If the ping fails 5 times consecutively, it will attempt to restart the network adapter up to 2 times, with a 60-second delay after each restart.
+
+.NOTES
+    Author: Pillars Zhang
+    Date: September 25, 2023
+#>
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$WatchInterface,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [string]$PingAddress,
+
+        [Parameter(Mandatory = $false)]
+        [int]$PingDelay = 10,
+
+        [Parameter(Mandatory = $false)]
+        [int]$MaxFailedAttempts = 3,
+
+        [Parameter(Mandatory = $false)]
+        [int]$MaxRestartAttempts = 3,
+
+        [Parameter(Mandatory = $false)]
+        [int]$RestartDelay = 30
+    )
+
+    $ErrorActionPreference = "Stop"
+
+    $NetIPInterface = (Get-NetIPConfiguration -InterfaceAlias $WatchInterface)
+    if ($NetIPInterface) {
+        $NetIPInterface
+    } else {
+        throw "Failed to find interface."
+    }
+
+    if (-not $PingAddress) {
+        $GatewayMode = $true
+    }
+
+    if ($GatewayMode) {
+        $PingAddress = $NetIPInterface.IPv4DefaultGateway.NextHop
+        Write-Host "The default IPV4 gateway for the ""$WatchInterface"" interface is ""$PingAddress"", but it will be reset before each ping."
+    }
+
+    try {
+        Test-Connection -ComputerName $PingAddress -Count 1
+    } catch {
+        Write-Host "An error occurred during early ping, but watchcat will continue to run: $($_.Exception.Message)"
+    }
+
+    if ($GatewayMode) {
+        Write-Host "Keep watching ""$WatchInterface"" on gateway ..."
+    } else {
+        Write-Host "Keep watching ""$WatchInterface"" on ""$PingAddress"" ..."
+    }    
+
+    $failedAttempts = 0
+    $restartAttempts = 0
+
+    while ($true) {
+        if ($GatewayMode) {
+            $PingAddress = $NetIPInterface.IPv4DefaultGateway.NextHop
+        }
+
+        if ($PingAddress) {
+            $pingResult = Test-Connection -ComputerName $PingAddress -Count 1 -Quiet
+            if ($pingResult) {
+                if ($restartAttempts -gt 0) {
+                    Write-Host "Connection has been recovered."
+                }
+                $failedAttempts = 0
+                $restartAttempts = 0
+            } else {
+                $failedAttempts++
+                Write-Host "Ping $PingAddress failed ($failedAttempts/$MaxFailedAttempts)"
+            }
+        } else {
+            $failedAttempts++
+            Write-Host "Bad interface or no address ($failedAttempts/$MaxFailedAttempts)"
+        }
+
+        if ($failedAttempts -ge $MaxFailedAttempts) {
+            $restartAttempts++
+            if ($restartAttempts -le $MaxRestartAttempts) {
+                Write-Host "Restarting network adapter ($restartAttempts/$MaxRestartAttempts)"
+                Restart-NetAdapter -InterfaceAlias $WatchInterface -Confirm:$false
+                Write-Host "Sleep $RestartDelay seconds ..."
+                Start-Sleep -Seconds $RestartDelay
+                $failedAttempts = 0
+            } else {
+                throw "Failed to recover connection. Stop watching."
+            }
+        } else {
+            Start-Sleep -Seconds $PingDelay
+        }
+        
+    }
+}
+
+Export-ModuleMember -Function Watch-Cat
